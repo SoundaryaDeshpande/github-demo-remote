e393d40340e31224757bfb1028a33ce609367224
soundaryadeshpande <soundarya_deshpande@persistent.com>
2022-01-30 15:40:27 +0530

    added function headers

--- demo.py
+++ demo.py
@@ -1,9 +1,13 @@
+# addition implementation
 def add(x,y):
 	pass
+#subtraction implementation
 def sub(x,y):
 	pass
+#multiplication implementation
 def mul(x,y):
 	pass
+#division implementation
 def div(x,y):
 	pass
 	
